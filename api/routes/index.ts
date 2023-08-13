19372e27d7091c96905148d6a9da227feae9d55d
BitR13x <bitr13x@protonmail.com>
2023-08-13 19:40:34 +0200

    removed API from front-end

--- api/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req, res) => {
-  return res.send({message: "api is working fine" });
-});
-
-export { router as Router }
